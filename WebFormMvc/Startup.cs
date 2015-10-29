2b60352208992096dff6a95d2a1badafc0900210
knolegsb <seon2you@hotmail.com>
2015-10-28 20:47:26 -0700

    start

---
+++ WebFormMvc/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormMvc.Startup))]
+namespace WebFormMvc
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
